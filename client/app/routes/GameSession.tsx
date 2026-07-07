20135be30d838a21a62491eb6e4b92d59ea1bbda
Foster Houlston <179643299+FosterHoulston@users.noreply.github.com>
2026-07-06 23:44:21 -0700

    feat(client): add skeleton of routing files for GameSession compoonent

--- client/app/routes/GameSession.tsx
+++ client/app/routes/GameSession.tsx
@@ -1,1 +1,9 @@
-//import type { Route } from "./+types/home";
+import type { Route } from "./+types/GameSession";
+import { GameSession } from "../GameSession/GameSession";
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "VimType" }, { name: "description", content: "GameSession page reached!" }];
+}
+
+export default function GameSession() {
+  return <GameSession />;
+}
