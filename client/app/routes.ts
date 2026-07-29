9567d36e68c8d055da70a10f929cb89d08350497
Foster Houlston <179643299+FosterHoulston@users.noreply.github.com>
2026-07-29 16:09:58 -0700

    feat(client): add empty pages and routing for navbar

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -3,4 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("game-session", "routes/game-session/game-session.tsx"),
+  route("game-info", "routes/game-info.tsx"),
+  route("leaderboards", "routes/leaderboards.tsx"),
+  route("profile", "routes/profile.tsx"),
 ] satisfies RouteConfig;
---
+++ client/app/routes/game-info.tsx
@@ -0,0 +1,6 @@
+export default function GameInfoRoute() {
+  return (
+    // TODO
+    <p>This page displays game rules</p>
+  );
+}
